4d6417b1730b21229f8db7ace469dc45a00b5cca
Johnathan Brooks <jbrooks@serverlessanalytics.io>
2018-07-16 02:10:17 -0700

    Updated deployment lambda function

--- upload-site-lambda.py
+++ upload-site-lambda.py
@@ -3,17 +3,20 @@ import io
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
 
-site_bucket = s3.Bucket('serverlessanalytics.io')
-build_bucket = s3.Bucket('build.serverlessanalytics.io')
+    site_bucket = s3.Bucket('serverlessanalytics.io')
+    build_bucket = s3.Bucket('build.serverlessanalytics.io')
 
-build_zip = io.BytesIO()
-build_bucket.download_fileobj('ServerlessAnalyticsBuild', build_zip)
+    build_zip = io.BytesIO()
+    build_bucket.download_fileobj('ServerlessAnalyticsBuild', build_zip)
 
-with zipfile.ZipFile(build_zip) as myzip:
-    for name in myzip.namelist():
-        obj = myzip.open(name)
-        site_bucket.upload_fileobj(obj, name,
-        ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
-        site_bucket.Object(name).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(build_zip) as myzip:
+        for name in myzip.namelist():
+            obj = myzip.open(name)
+            site_bucket.upload_fileobj(obj, name,
+            ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+            site_bucket.Object(name).Acl().put(ACL='public-read')
+
+    return 'Lambda function called to deploy site.'
