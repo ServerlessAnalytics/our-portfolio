f60913cf61bf6a3434aa38723ae3c60ba932b255
Johnathan Brooks <jbrooks@serverlessanalytics.io>
2018-07-16 01:47:23 -0700

    Add upload site Python script

---
+++ upload-site-lambda.py
@@ -0,0 +1,19 @@
+import boto3
+import io
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+
+site_bucket = s3.Bucket('serverlessanalytics.io')
+build_bucket = s3.Bucket('build.serverlessanalytics.io')
+
+build_zip = io.BytesIO()
+build_bucket.download_fileobj('ServerlessAnalyticsBuild', build_zip)
+
+with zipfile.ZipFile(build_zip) as myzip:
+    for name in myzip.namelist():
+        obj = myzip.open(name)
+        site_bucket.upload_fileobj(obj, name,
+        ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+        site_bucket.Object(name).Acl().put(ACL='public-read')
